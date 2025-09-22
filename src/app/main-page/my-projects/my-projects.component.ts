3b756bcd439ae3ec2557d1c7c92c2b8bcb960153
Magda Danielyan <mdanielyan50@gmail.com>
2025-09-23 00:19:50 +0200

    add:selected project still activ

--- src/app/main-page/my-projects/my-projects.component.ts
+++ src/app/main-page/my-projects/my-projects.component.ts
@@ -1,11 +1,12 @@
 import { Component} from '@angular/core';
 import {SingleComponent} from './single/single.component';
 import { TranslateModule } from "@ngx-translate/core";
+import { NgClass } from '@angular/common';
 
 
 @Component({
   selector: 'app-my-projects',
-  imports: [SingleComponent,TranslateModule],
+  imports: [SingleComponent,TranslateModule,NgClass],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss'
 })
@@ -44,20 +45,6 @@ gitHub:"img/skills_img/Button Secondary Web (1).svg",
 },
 {
 img1: "img/Ellipse 8.png",
-par1: "About the project3 ",
-description1:" Duration: 3 Weeks",
-text1: "This App is a Slack Clone App.It revolutionizes team communication and collaboration with its intuitive interface,real-time messaging, and robust channel organization.",
-par2: "How I have organised my work process3",
-description2: "How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.",
-par3: "My group work experience3",
-description3:"How many people were in the team and what was your role? Describe your tasks in 1-2 sentences, for example: login form, dashboard or chat functionality. What technologies did you use? It is nice to mention good teamwork and cooperation.",
-right_paar:"Technologies3",
-right_project_pic:"img/daBubble.png",
-live_test:"img/skills_img/Button Primary Web.svg" ,
-gitHub:"img/skills_img/Button Secondary Web (1).svg",
-},
-{
-img1: "img/Ellipse 8.png",
 par1: "About the project4 ",
 description1:" Duration: 5 Weeks",
 text1: "This App is a Slack Clone App.It revolutionizes team communication and collaboration with its intuitive interface,real-time messaging, and robust channel organization.",
@@ -70,11 +57,37 @@ right_project_pic:"img/join.png",
 live_test:"img/skills_img/Button Primary Web.svg" ,
 gitHub:"img/skills_img/Button Secondary Web (1).svg",
 },
+{
+img1: "img/Ellipse 8.png",
+par1: "About the project3 ",
+description1:" Duration: 3 Weeks",
+text1: "This App is a Slack Clone App.It revolutionizes team communication and collaboration with its intuitive interface,real-time messaging, and robust channel organization.",
+par2: "How I have organised my work process3",
+description2: "How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.",
+par3: "My group work experience3",
+description3:"How many people were in the team and what was your role? Describe your tasks in 1-2 sentences, for example: login form, dashboard or chat functionality. What technologies did you use? It is nice to mention good teamwork and cooperation.",
+right_paar:"Technologies3",
+right_project_pic:"img/daBubble.png",
+live_test:"img/skills_img/Button Primary Web.svg" ,
+gitHub:"img/skills_img/Button Secondary Web (1).svg",
+},
+
   ]
 
 selectedProject = this.projectlist[0];
+selectedProjectIndex = 0;
+ hoveredIndex: number | null = null; 
+
  selectProject(index: number) {
     this.selectedProject = this.projectlist[index];
+    this.selectedProjectIndex = index;
+  }
+   onProjectHover(index: number) {
+    this.hoveredIndex = index;
+  }
+
+  onProjectLeave() {
+    this.hoveredIndex = null;
   }
 
 }
